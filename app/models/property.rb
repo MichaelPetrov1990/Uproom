b7f6f33873e53cc656b220177e518e40b3bfea10
Michael Petrov <mpetrovcello@gmail.com>
2021-02-15 10:09:09 +0000

    add some basic validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,11 @@
 class Property < ApplicationRecord
-  
+  validates :phone, presence: true
+  validates :beds, presence: true
+  validates :beds, numericality: true
+  validates :bathrooms, presence: true
+  validates :bathrooms, numericality: true
+  validates :address, presence: true
+  validates :email, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
